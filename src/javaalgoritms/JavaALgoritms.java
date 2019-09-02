728d39c7e5418c65736f60c6d23b485cd2fc13b0
BRIAN <BRIAN@DESKTOP-FKUAPU1>
2019-09-02 10:07:08 -0500

    Proyecto prueba v2

---
+++ src/javaalgoritms/JavaALgoritms.java
@@ -0,0 +1,18 @@
+
+package javaalgoritms;
+
+import java.util.Scanner;
+
+public class JavaALgoritms{ 
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+       
+        char letra;
+        System.out.println("Digite una caracter: ");
+        letra=entrada.next().charAt(0);
+               
+        System.out.println("La cadena es: "+entrada);
+        
+    }
+}
+
